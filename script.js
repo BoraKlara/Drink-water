8d952a68d84ba06f054cbb5c472fd802725e4ab0
B.Klara Simon <borbala.klara@gmail.com>
2021-08-10 17:52:25 +0200

    add variables

---
+++ script.js
@@ -0,0 +1,5 @@
+const smallCups = document.querySelectorAll(".cup-small");
+
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
