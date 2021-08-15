e9a22169a6d7a67e6be4b9ebd89ce7773946d91a
B.Klara Simon <borbala.klara@gmail.com>
2021-08-15 21:52:56 +0200

    fix bug

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
+prompt("hello");
+
 const smallCups = document.querySelectorAll(".cup-small");
 
 const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
 
-smallCups.forEach((cup, i) => {
-  cup.addEventListener('click', () => highlightCups)
-};
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highlightCups(idx));
+});
 
-function highlightCups(i){
-  smallCups.forEach((cup, i2) =>){
-    if(i2 <= i){
-      cup.classList.add('full')
-    }else{
-      cup.classList.remove('full')
+function highlightCups(idx) {
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
     }
-  }
+  });
 }
